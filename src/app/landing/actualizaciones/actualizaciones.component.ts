359d8177b6482ffab0af6e5faad8a41bdca8ca41
Matias Portela <matiasportela94@gmail.com>
2024-05-05 19:43:44 -0300

    fixed sections

--- src/app/landing/actualizaciones/actualizaciones.component.ts
+++ src/app/landing/actualizaciones/actualizaciones.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActualizacionesComponent implements OnInit {
 
+  certificateIconPath:string = "/assets/images/icons/certificate.png";
+  quickClockIconPath:string = "/assets/images/icons/other-quick-clock.png";
+  pointsIconPath:string = "/assets/images/icons/plus-points.png";
+  diplomaHeaderImage:string = "/assets/images/licenciaturas.png";
+  mapaDiplomaturaImage:string ="/assets/images/mapa-diplomatura.png";
+  escarapelaIconPath:string ="/assets/images/icons/escarapela.svg";
+  onlineIconPath:string ="/assets/images/online.png";
+  
+  
   constructor() { }
 
   ngOnInit(): void {
